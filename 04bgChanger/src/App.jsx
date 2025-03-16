c7beb1c41b32c9e1435b43aa6d2c5ecf85447520
Harshal Bhosale <134089001+codehb01@users.noreply.github.com>
2025-03-16 10:51:36 +0530

    react commit

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,17 +1,98 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+// function App() {
+//   const [color, setColor] = useState("olive");
+
+//   return (
+//     <>
+//       <div
+//         className="w-full h-screen duration-200"
+//         style={{ backgroundColor: color }}
+//       >
+//         <div className="fixed flex flex-wrap justify-center bottom-25 inset-x-0 px-2">
+//           <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white py-2 rounded-3xl px-3  ">
+//             <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg m-4">
+//               Blue
+//             </button>
+//             <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg m-4">
+//               Red
+//             </button>
+//             <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg m-4">
+//               Green
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
-      <h1 className="bg-green-400 p-2 rounded-md text-3xl">
-        Background colour changing React + Vite app
-      </h1>
-    </>
+    <div
+      className="w-full h-screen duration-200"
+      style={{
+        backgroundColor: color,
+        margin: 0,
+        padding: 0,
+        maxWidth: "100%",
+        textAlign: "left",
+      }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white py-2 rounded-3xl px-3">
+          <button
+            onClick={() => setColor("blue")}
+            style={{
+              backgroundColor: "blue",
+              color: "white",
+              borderRadius: "9999px", // full rounded
+              padding: "0.25rem 1rem",
+              border: "none",
+              margin: "0.5rem",
+            }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("red")}
+            style={{
+              backgroundColor: "red",
+              color: "white",
+              borderRadius: "9999px", // full rounded
+              padding: "0.25rem 1rem",
+              border: "none",
+              margin: "0.5rem",
+            }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            style={{
+              backgroundColor: "green",
+              color: "white",
+              borderRadius: "9999px", // full rounded
+              padding: "0.25rem 1rem",
+              border: "none",
+              margin: "0.5rem",
+            }}
+          >
+            Green
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ 05passwordgenerator/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useCallback, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPasswordAllowed] = useState("");
+
+  const copyPassword = () => {
+    window.navigator.clipboard.writeText(password);
+  };
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numberAllowed) str += "123456789";
+    if (characterAllowed) str += "!@#$%^&*()_+";
+
+    for (let ex = 1; ex < length; ex++) {
+      const char = Math.floor(Math.random() * str.length - 1);
+      pass += str.charAt(char);
+    }
+
+    setPasswordAllowed(pass);
+    // here in dependency array we are passing values which are been check here in useCallback
+  }, [length, characterAllowed, numberAllowed]);
+
+  //all the methods that you want to chnage or re-render are pass their varibale name  in dependency array here
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, characterAllowed]);
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800  text-orange-500">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden">
+          <input
+            type="text"
+            value={password}
+            name=""
+            id=""
+            className=" outline-none w-full rounded-l-md bg-white  mt-6 py-1 px-3 read-only:"
+            readOnly
+          />
+          <button
+            onClick={copyPassword}
+            className="rounded-r-md bg-purple-500 py-1 px-3 mt-6 text-white "
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex items-center gap-x-1 mt-2">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            name=""
+            id=""
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+          />
+          <label htmlFor="length">Length: {length}</label>
+
+          <div className="flex items-center gap-x-1 ml-4">
+            <input
+              type="checkbox"
+              name="number"
+              id=""
+              defaultChecked={numberAllowed}
+              onChange={(prev) => !prev}
+            />
+            <label htmlFor="number">Number</label>
+            <div className="flex items-center gap-x-1 ml-4">
+              <input
+                type="checkbox"
+                name="number"
+                id=""
+                defaultChecked={numberAllowed}
+                onChange={(prev) => !prev}
+              />
+              <label htmlFor="character">Character</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
